d5ff836c389fba6d3039db1d963cea26f27bed46
onixuniverse <warofmyhome@gmail.com>
2022-02-04 15:17:07 +0500

    rename bot.py to terry.py

--- terry/bin/src/terry.py
+++ terry/bin/src/terry.py
@@ -10,32 +10,27 @@ from terry.resources.data.config import OWNER_IDS, PREFIX
 COGS = [path.split('\\')[-1][:-3] for path in glob('terry/bin/cogs/*.py')]
 
 
-class Bot(Bot):
+class Terry(Bot):
     def __init__(self):
         self.PREFIX = PREFIX
         super().__init__(command_prefix=PREFIX, OWNER_IDS=OWNER_IDS, intents=Intents.all(), help_command=None)
 
     def setup(self):
-        logger.info('Setting cogs...')
+        logger.info('Loading cogs...')
 
         for cog in COGS:
             try:
-                self.load_extension(f'bin.cogs.{cog}')
-                logger.info(f'{cog} setup!')
+                self.load_extension(f'terry.bin.cogs.{cog}')
+                logger.info(f'{cog} loaded!')
             except ExtensionNotLoaded:
                 logger.error(ExtensionNotLoaded)
 
-    def run(self, version):
-        self.VERSION = version
-
-        with open('terry/resources/data/tokens/token', 'r') as tf:
-            self.TOKEN = tf.read()
-
+    def run(self, bot_token):
         self.setup()
-        super().run(self.TOKEN, reconnect=True)
+        super().run(bot_token, reconnect=True)
 
     async def on_ready(self):
-        logger.info('Bot started!')
+        logger.info('Bot successfully started!')
 
 
-_bot = Bot()
+bot = Terry()
